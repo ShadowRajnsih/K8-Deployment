b694d095f83ec9a7c70d35f381488006703f4ac4
Rajnish <rr66520@gmail.com>
2021-09-06 16:21:54 +0530

    rectified the errors

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-kubectl create -f /deployment_files/voting_app_deployment.yaml
-kubectl create -f /service_files/voting_app_service.yaml
+kubectl create -f deployment_files/voting_app_deployment.yaml
+kubectl create -f service_files/voting_app_service.yaml
 
-kubectl create -f /deployment_files/redis_deployment.yaml
-kubectl create -f /service_files/redis_service.yaml
+kubectl create -f deployment_files/redis_deployment.yaml
+kubectl create -f service_files/redis_service.yaml
 
-kubectl create -f /deployment_files/postgres_app_deployment.yaml
-kubectl create -f /service_files/postgres_service.yaml
+kubectl create -f deployment_files/postgres_deployment.yaml
+kubectl create -f service_files/postgres_service.yaml
 
-kubectl create -f /deployment_files/worker_app_deployment.yaml
+kubectl create -f deployment_files/worker_app_deployment.yaml
 
-kubectl create -f /deployment_files/result_app_deployment.yaml
-kubectl create -f /service_files/result_app_service.yaml
+kubectl create -f deployment_files/result_app_deployment.yaml
+kubectl create -f service_files/result_app_service.yaml
